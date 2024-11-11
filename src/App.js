5ff9d50a1707e137c4f6ff3b1d8ce824071f3ff4
Izaak-Grinner <kusk_0619@outlook.jp>
2024-11-11 22:38:45 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+// App.js
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './contents/Home'
+import Home from './contents/Home';
 import About from './contents/About';
 import Recommend from './contents/Recommend';
+import { Routes, Route, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lugal-hasibiroのサイト</h1>
+        <h1>hasibiroのサイト</h1>
         <nav>
           <Link to="/">Home</Link> |
           <Link to="/about">About</Link> |
-          <Link to="/recommend">広告</Link> {/* 新しいリンクを追加 */}
+          <Link to="/recommend">広告欄</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/recommend" element={<Recommend />} /> {/* 新しいルートを追加 */}
+          <Route path="/recommend" element={<Recommend />} />
         </Routes>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; {/* ここでdefaultエクスポートを確認 */ }

765ccce333c6d06e94ee9ad7cfa609c69144ea59
Izaak-Grinner <kusk_0619@outlook.jp>
2024-11-17 11:33:19 +0900

    play追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './contents/Home';
 import About from './contents/About';
 import Recommend from './contents/Recommend';
+import PlayRoom from './contents/PlayRoom';
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -14,12 +15,14 @@ function App() {
           <Link to="/">Home</Link> |
           <Link to="/about">About</Link> |
           <Link to="/recommend">広告欄</Link>
+          <Link to="/playroom">play</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/recommend" element={<Recommend />} />
+          <Route path="/playroom" element={<PlayRoom />} />
         </Routes>
       </header>
     </div>
---
+++ src/contents/PlayRoom.jsx
@@ -0,0 +1,10 @@
+const PlayRoom = () => {
+    return (
+        <div>
+            <h2>遊ぼう</h2>
+            <p>こんなん見てるなら勉強しろ</p>
+        </div>
+    );
+};
+
+export default PlayRoom;

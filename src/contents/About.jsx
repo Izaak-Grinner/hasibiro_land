9f2adaef97529393700786000189c28efb0b0b68
Izaak-Grinner <kusk_0619@outlook.jp>
2024-11-19 00:25:49 +0900

    sourse追加

--- src/contents/About.jsx
+++ src/contents/About.jsx
@@ -4,6 +4,9 @@ const About = () => {
             <h2>このサイトって何？</h2>
             <p>受験生が勉強の合間に更新するサイトです。</p>
             <p>サイト制作はさぁ、OOOOなんだよ</p>
+            <p></p>
+            <a href="https://github.com/Izaak-Grinner/hasibiro_land">ソースコード(読めるもんなら読んでみろ)</a>
+
 
         </div>
     );
